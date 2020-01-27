352099a1800fa69d0c1baf67512443d93ca903fc
Tatiana Savonik <tatsiana.savonik@gmail.com>
2020-01-27 20:55:31 +0300

    Test

---
+++ src/jc01_2020/Savonik/Task1.java
@@ -0,0 +1,20 @@
+package jc01_2020.Savonik;
+
+/*
+ *
+ * С клавиатуры вводится натуральное число. Найти его наибольшую цифру
+ *
+ * Формат вывода (ошибки недопустимы):
+ * одно число
+ *
+ */
+
+import java.util.Scanner;
+
+public class Task1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		long input = scanner.nextLong();
+		
+	}
+}
