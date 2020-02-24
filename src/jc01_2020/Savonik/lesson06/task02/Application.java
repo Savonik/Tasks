46db90272796df7716146481834b3fb7abd4c680
Tatiana Savonik <tatsiana.savonik@gmail.com>
2020-02-25 02:57:29 +0300

    Готово

--- src/jc01_2020/Savonik/lesson06/task02/Application.java
+++ src/jc01_2020/Savonik/lesson06/task02/Application.java
@@ -4,13 +4,13 @@ package jc01_2020.Savonik.lesson06.task02;
 public class Application {
    public static void main(String[] args) {
       
-      Bancomat bancomat1 = new Bancomat(1, 1, 1);
-      System.out.println(bancomat1.balance());
-      bancomat1.addMoney(2,5,6);
-      System.out.println(bancomat1.balance());
-      bancomat1.addMoney(2,5,6);
-      System.out.println(bancomat1.balance());
-      bancomat1.withdraw(50);
-      System.out.println(bancomat1.balance());
+      Bancomat bancomat = new Bancomat(1, 1, 1);
+      System.out.println(bancomat.balance());
+      bancomat.addMoney(2,5,6);
+      System.out.println(bancomat.balance());
+      bancomat.addMoney(2,5,6);
+      System.out.println(bancomat.balance());
+      bancomat.withdraw(50);
+      System.out.println(bancomat.balance());
    }
 }
