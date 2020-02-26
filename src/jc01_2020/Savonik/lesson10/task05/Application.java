2b8a1cce2f2d2057de053a95ca4503585896ceef
Tatiana Savonik <tatsiana.savonik@gmail.com>
2020-02-27 02:50:19 +0300

    Готово

--- src/jc01_2020/Savonik/lesson10/task05/Application.java
+++ src/jc01_2020/Savonik/lesson10/task05/Application.java
@@ -12,13 +12,28 @@ package jc01_2020.Savonik.lesson10.task05;
  *
  */
 
+import java.util.*;
+
 public class Application {
 
-	public static void main(String[] args) {
-		// Объявить список
-		for (int i = 0; i < 5; i++) {
-			// Заполнить список
-		}
-	}
+    public static void main(String[] args) {
+        // Объявить список
+		TreeMap<String, List<String>> map = new TreeMap<>();
+        // Заполнить список
+        try (Scanner scanner = new Scanner(System.in)) {
+            for (int i = 0; i < 3; i++) {
+                String buyer = scanner.nextLine();
+                String ship = scanner.nextLine();
+                String amount = scanner.nextLine();
+                map.put(buyer, new ArrayList<>(Arrays.asList(ship, amount)));
+            }
+        }
+
+		for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+			System.out.println("Имя: "+entry.getKey());
+			System.out.println(entry.getValue().get(0)+" - "+entry.getValue().get(1));
+			}
+        
+    }
 
 }
