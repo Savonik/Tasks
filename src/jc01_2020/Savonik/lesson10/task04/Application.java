d8538478209b0157f2b4c1ec63ef7d7267b21214
Tatiana Savonik <tatsiana.savonik@gmail.com>
2020-02-27 00:03:29 +0300

    Готово

--- src/jc01_2020/Savonik/lesson10/task04/Application.java
+++ src/jc01_2020/Savonik/lesson10/task04/Application.java
@@ -7,16 +7,35 @@ package jc01_2020.Savonik.lesson10.task04;
  *
  */
 
-import java.util.Scanner;
+
+import java.awt.List;
+import java.security.KeyStore;
+import java.util.*;
 
 public class Application {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		for (int i = 0; i < 10; i++) {
-			String surname = scanner.nextLine();
-			String firstName = scanner.nextLine();
-		}
-	}
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            Map<String, String> map = new HashMap<>();
+            ArrayList<String> s = new ArrayList<>();
+            for (int i = 0; i < 6; i++) {
+                String surname = scanner.nextLine();
+                String firstName = scanner.nextLine();
+                map.put(surname, firstName);
+            }
+            System.out.println(map);
+            for (Map.Entry<String, String> entry1 : map.entrySet()) {
+                int i = 0;
+                for (Map.Entry<String, String> entry2 : map.entrySet()) {
+                    if (entry1.getValue().equals(entry2.getValue())) { i = i + 1; }
+                }
+                if (i >= 2) { s.add(entry1.getKey()); }
+            }
 
+            for (String value : s) {
+                map.remove(value);
+            }
+            System.out.println("map = " + map);
+        }
+    }
 }
