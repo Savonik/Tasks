f9498b08e64c1149608cd3baa46d06005d47e6cd
Tatiana Savonik <tatsiana.savonik@gmail.com>
2020-02-27 04:56:46 +0300

    Готово

--- src/jc01_2020/Savonik/lesson08/task03/Application.java
+++ src/jc01_2020/Savonik/lesson08/task03/Application.java
@@ -32,61 +32,77 @@ package jc01_2020.Savonik.lesson08.task03;
 public class Application {
     
     public static class Date {
-        private  static int day, month, year,n,k,m;
+        private  static int day;
+        private  static int month;
+        private  static int year;
+        private  static int getDayOfYear;
+        private  static int getDays;
         boolean leapYear;
 
         public Date(int day, int month, int year){
-            this.day=day;
-            this.month=month;
-            this.year=year;
+            Date.day =day;
+            Date.month =month;
+            Date.year =year;
             leapYear = new Year().leapYear();
+            getDayOfYear = new Year().getDayOfYear();
             System.out.println("Заданная дата: "+day+"."+month+"."+year);
         }
-
-        public DayOfWeek getDayOfWeek() {
+        
+        public static DayOfWeek valueOf (int index){
             DayOfWeek Day = null;
-            k = year - (14 - month) / 12;
-            m = month + 12 * ((14 - month) / 12) - 2;
-            n = ((day + (31 * m) / 12 + k + k / 4 - k / 100 + k / 400) % 7);
             for (DayOfWeek s: DayOfWeek.values()) {
-                if (s.getNumber() == n){ Day = s; }
+                if (s.getNumber() == index){  Day = s; }
             }
-            return Day;
-        }
-
-        public int daysBetween(Date startDate, Date endDate){
-            
-            
-            return 1;
-        }
-
-        static class Year{
-            public boolean leapYear(){
-                if (((year%4==0)&&(year%100!=0))||(year%400==0))  {return  true;}
-                else { return false;}
-            }
-            public int getDayOfYear(){
-                if (leapYear())  {return  366;}
-                else { return 365;}
-            }
-
+            return  Day;
         }
         
-        static class Month{
+        public DayOfWeek getDayOfWeek() {
+            int k = year-(14-month)/12;
+            int n = (day+31*(month+12*((14-month)/12)-2)/12+k+k/4-k/100+k/400)%7;
+           return valueOf(n);
+        }
+
+        public int daysBetween(Date endDate){
+            return 1;
+        }
+        
+        public boolean leapYear() {
+            return leapYear;
+        }
+        public int getDayOfYear() {
+            return getDayOfYear;
+        }
+        
+        static class Year{
+            public boolean leapYear(){
+                return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+            }
+            private int getDayOfYear(){
+                if (!leapYear()) { return 365;} else {return  366;}
+            }
+            
+        }
+        
+       static class Month{
+            public int getDays(int monthNumber, boolean leapYear){
+                if (monthNumber==2){ return leapYear?28:29; }
+                else return monthNumber%2==1?31:30;
+            }
         }
 
         static class Day{
         }
-
-    }
+        public static void main(String[] args) {
+            Date date1=new Date(28,2,2020);
+            Date date2 = new Date(30,5,2023);
+            System.out.println("День недели-"+ date1.getDayOfWeek());
+            System.out.println("Год високосный: "+date1.leapYear());
+            System.out.println("Количество дней в году: "+date1.getDayOfYear());
+            Date.Month month1= new Application.Date.Month();
+            System.out.println(month1.getDays(1,true));
+            date1.daysBetween(date2);
+        }
     
-
-    public static void main(String[] args) {
-        Date date1=new Date(24,2,2020);
-        System.out.println(date1.getDayOfWeek());
-        Date.Year year = new Date.Year();
-        System.out.println(year.leapYear());
-        System.out.println(year.getDayOfYear());
     }
 }
 
--- src/jc01_2020/Savonik/lesson08/task03/Month.java
+++
@@ -1,5 +0,0 @@
-package jc01_2020.Savonik.lesson08.task03;
-
-public enum Month {
-    
-}
