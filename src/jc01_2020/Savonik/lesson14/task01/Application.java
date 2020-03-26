3660b1b6640e354d0d221a478ab1b24693ef3772
Tatiana Savonik <tatsiana.savonik@gmail.com>
2020-03-26 15:21:26 +0300

    Готово

---
+++ src/jc01_2020/Savonik/lesson14/task01/Application.java
@@ -0,0 +1,24 @@
+package jc01_2020.Savonik.lesson14.task01;
+
+/*
+ *
+ * При помощи FileWriter создать в папке lesson14/resource текстовый файл с именем lesson14-01.txt, записать в него строки
+ * "Давай проверим, что мы умеем писать в файл.
+ * Поехали!"
+ *
+ */
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Application {
+
+	public static void main(String[] args) throws IOException {
+		FileWriter writer = new FileWriter("src\\jc01_2020\\Savonik\\lesson14\\resource\\lesson14-01.txt");
+		writer.write("Давай проверим, что мы умеем писать в файл");
+		writer.write("\n");
+		writer.write("Поехали");
+		writer.close();
+	}
+
+}
