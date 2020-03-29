7a9994b38ad0d5f153d620e54f8229087c46414f
Tatiana Savonik <tatsiana.savonik@gmail.com>
2020-03-30 01:28:37 +0300

    Готово

--- src/jc01_2020/Savonik/lesson14/task02/Application.java
+++ src/jc01_2020/Savonik/lesson14/task02/Application.java
@@ -7,7 +7,6 @@ package jc01_2020.Savonik.lesson14.task02;
  */
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Application {
@@ -18,29 +17,26 @@ public class Application {
 		StringBuilder s2 = new StringBuilder();
 		try {
 			FileReader reader = new FileReader("src\\jc01_2020\\Savonik\\lesson14\\resource\\lesson14-01.txt");
-			Scanner scanner = new Scanner(reader);
 			while (reader.ready()) {
 				s1.append((char) reader.read());
 			}
 
 			System.out.println(s1);
 			reader.close();
-			scanner.close();
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-
-
+			
+		} catch (IOException e) { System.out.println(e.getMessage()); }
+		
 		try {
 			FileInputStream fis = new FileInputStream("src\\jc01_2020\\Savonik\\lesson14\\resource\\lesson14-01.txt");
 			InputStreamReader iR = new InputStreamReader(fis, "Windows-1251");
-			while (iR.ready()) {
-				s2.append((char) iR.read());
-			}
+			while (iR.ready()) { s2.append((char) iR.read()); }
 			fis.close();
+			iR.close();
 			System.out.println(s2);
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
+			
+		} catch (IOException e) { System.out.println(e.getMessage()); }
+		
 		System.out.println(s1.toString().equals(s2.toString()));
-	}}
+
+	}
+}
